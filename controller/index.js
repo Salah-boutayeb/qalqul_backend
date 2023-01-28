802bad069ff8d37c8b61f4d0ae7e0c04413c3c71
Salah-boutayeb <boutayebsalaheddin@gmail.com>
2023-01-28 11:46:51 +0100

    initale webhook

--- controller/index.js
+++ controller/index.js
@@ -24,7 +24,7 @@ const getPost = async (req, res) => {
 const webhook = (req, res) => {
   console.log(req.body);
   if (req.body.object === "page") {
-    req.body.entry.forEach((entry) => {
+    req.body.changes.forEach((entry) => {
       entry.messaging.forEach((event) => {
         if (event.message && event.message.text) {
           // sendMessage(event);
